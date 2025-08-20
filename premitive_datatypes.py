39d7b3ada9e853fec2663fbed927ea15a2fc5bd5
Thottambedu Ajay Kumar <ajaykumarthottambedu@gmail.com>
2025-08-20 22:11:13 +0530

    Add files via upload

---
+++ premitive_datatypes.py
@@ -0,0 +1,54 @@
+#data types
+
+'''#premitive
+1- int-()-(10, 20, -1, -1)
+2- float-()-2.3
+3- complex-1+3j, 1=real, 3j=imagenary
+4- bollean- 0-False,  1-true
+'''
+
+##int()
+int=(24, 18)
+print(int)
+
+##bollean
+a = 0
+b = 1
+print(a == b)
+print(a <= b)
+
+##float()
+
+a = 2.0
+print(type(a))
+
+##complex()
+v=2+3j
+print(type(v))
+
+
+##Type covensation
+'''int(), float(), bool(), str()'''
+
+#int to float
+a = 24
+print(float(a))
+
+#float to int
+b = 5.5
+print(float(b))
+
+''' data lose = explicit type conversion
+no  data lose = implicit type conversion'''
+
+
+
+
+
+
+
+
+
+
+
+
